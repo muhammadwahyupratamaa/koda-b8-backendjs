20c80a9cf299f9a304c726db89f2abbeae82d5a0
Muhammad Wahyu Pratama <muhammad123wahyu123@gmail.com>
2026-08-04 14:15:21 +0700

    feat: add category route to main router

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,14 @@
 import { Router } from "express";
 import authRoute from "./auth.route.js";
 import productRoute from "./product.route.js";
+import categoryRoute from "./category.route.js"
 
 const router = Router();
 
 router.use("/auth", authRoute);
 router.use("/products", productRoute);
+router.use("/categories", categoryRoute)
+
 
 
 export default router;
