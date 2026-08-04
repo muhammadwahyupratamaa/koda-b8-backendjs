a60e033800c47c834998bfa732d96a9787ff5cf5
Muhammad Wahyu Pratama <muhammad123wahyu123@gmail.com>
2026-08-04 11:39:36 +0700

    feat: create categories table migration

---
+++ migrations/20260804042949_create_categories.up.sql
@@ -0,0 +1,6 @@
+CREATE TABLE "categories"(
+    "id" BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    "name" VARCHAR(255) NOT NULL ,
+    "created_at" TIMESTAMP DEFAULT NOW(),
+    "updated_at" TIMESTAMP DEFAULT NOW()
+);
