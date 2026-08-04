89756830f38c5386af11366949900403147d4ea4
Muhammad Wahyu Pratama <muhammad123wahyu123@gmail.com>
2026-08-05 03:09:06 +0700

    feat: implement checkout management

---
+++ src/routes/checkout.route.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import checkoutController from "../controllers/checkout.controller.js";
+import authMiddleware from "../middlewares/auth.middleware.js";
+
+const router = Router();
+
+router.use(authMiddleware);
+
+router.post("/", checkoutController.checkout);
+router.get("/orders", checkoutController.getOrders);
+
+export default router;
