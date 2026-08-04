24e07b6841cca9a7e610a4f008dd5c8404799aff
Muhammad Wahyu Pratama <muhammad123wahyu123@gmail.com>
2026-08-04 14:14:57 +0700

    feat: add seed categories migration files for up and down operations

---
+++ migrations/20260804061402_seed_categories.down.sql
@@ -0,0 +1,9 @@
+DELETE FROM categories
+WHERE name IN (
+    'Elektronik',
+    'Fashion',
+    'Rumah & Dapur',
+    'Kecantikan',
+    'Olahraga',
+    'Buku & Alat Tulis'
+);
---
+++ migrations/20260804061402_seed_categories.up.sql
@@ -0,0 +1,10 @@
+INSERT INTO "categories" (name)
+VALUES
+('Elektronic'),
+('Fashion'),
+('Rumah & Dapur'),
+('Kecantikan'),
+('Olahraga'),
+('Buku & Alat tulis');
+
+SELECT * FROM "categories";
