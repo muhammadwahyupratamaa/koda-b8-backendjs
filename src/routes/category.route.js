d06248fbe53c884ea044e2b451225167def9d634
Muhammad Wahyu Pratama <muhammad123wahyu123@gmail.com>
2026-08-05 13:32:34 +0700

    feat: add OpenAPI documentation for categories route

--- src/routes/category.route.js
+++ src/routes/category.route.js
@@ -3,6 +3,45 @@ import categoryController from "../controllers/category.controller.js";
 
 const router = Router();
 
+/**
+ * @openapi
+ * tags:
+ *   name: Categories
+ *   description: Category Management API
+ */
+
+/**
+ * @swagger
+ * /categories:
+ *   get:
+ *     summary: Get all categories
+ *     description: Retrieve a list of all product categories.
+ *     tags: [Categories]
+ *     responses:
+ *       200:
+ *         description: Categories retrieved successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   example: true
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       id:
+ *                         type: integer
+ *                         example: 1
+ *                       name:
+ *                         type: string
+ *                         example: Shoes
+ *       500:
+ *         description: Internal server error
+ */
 router.get("/", categoryController.getAll);
 
 export default router;
